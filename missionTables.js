bdd7244bd9c6cfce3b184fdeea6f7523958cc234
Cmoore <charlesmooreguard-git@yahoo.com>
2017-08-08 20:54:24 -0400

    Added missions for Free Traders
    Modified index to show Freed Trader and Agent missions

--- missionTables.js
+++ missionTables.js
@@ -21,9 +21,10 @@ var complication = function(c, d){
 	};
 };
 
-var mission = function(m, d, c, b, e) {
+var mission = function(t, m, d, c, b, e) {
 	var that = this;
 
+	this.target = t;
 	this.missionType = m;
 	this.destination = d;
 	this.numComplications = c;
@@ -45,10 +46,9 @@ var mission = function(m, d, c, b, e) {
 	};
 	
 	this.print = function() {
-		debug(String.format(headerTemplate, "Mission"));
+		debug(String.format(headerTemplate, "Mission: " + that.target));
 		debug(String.format(cellTemplate, "Type", that.missionType));
 		debug(String.format(cellTemplate, "Destination", that.destination));
-		debug(String.format(cellTemplate, "# Complications", that.numComplications));
 		debug(String.format(cellTemplate, "Base Reward", that.baseReward));
 		debug(String.format(cellTemplate, "Extra Reward", that.extraReward));
 		debug(String.format(cellTemplate, "Employer", that.employer));
@@ -57,6 +57,7 @@ var mission = function(m, d, c, b, e) {
 		debug(String.format(cellTemplate, "Mission Description", that.description));
 		debug(String.format(cellTemplate, "Location", that.location));
 		debug(String.format(cellTemplate, "location Description", that.locationDescription));
+//		debug(String.format(cellTemplate, "# Complications", that.numComplications));
 		$.each(that.complications, function(c,i){
 			debug(String.format(cellTemplate, "Complication", this.complication));
 			debug(String.format(cellTemplate, "Complication Description", this.complicationDescription));			
@@ -65,7 +66,7 @@ var mission = function(m, d, c, b, e) {
 }
 
 // Table 18
-var missionType = function() {
+var missionType = function(target) {
 	var r = "" + roll() + roll();
 	switch (r) {
 	case "11":
@@ -73,63 +74,63 @@ var missionType = function() {
 	case "13":
 	case "14":
 	case "15":
-		return new mission("Routine", "Within System", 0, roll(), 0);
+		return new mission(target, "Routine", "Within System", 0, roll(), 0);
 	case "16":
 	case "21":
 	case "22":
 	case "23":
-		return new mission("Routine", "Within System", 1, roll() + 2, 0);
+		return new mission(target, "Routine", "Within System", 1, roll() + 2, 0);
 	case "24":
 	case "25":
-		return new mission("Easy", "Within System", 0, roll() + roll(), 0);
+		return new mission(target, "Easy", "Within System", 0, roll() + roll(), 0);
 	case "26":
 	case "31":
-		return new mission("Easy", "Within System", 1, roll() + roll(), 1);
+		return new mission(target, "Easy", "Within System", 1, roll() + roll(), 1);
 	case "32":
 	case "33":
-		return new mission("Easy", "Within System", 1, roll() + roll() + 2, 1);
+		return new mission(target, "Easy", "Within System", 1, roll() + roll() + 2, 1);
 	case "34":
 	case "35":
 	case "36":
 	case "41":
 	case "42":
 	case "43":
-		return new mission("Normal", "Within System", 1, roll() + roll()
+		return new mission(target, "Normal", "Within System", 1, roll() + roll()
 				+ roll(), 0);
 	case "44":
 	case "45":
-		return new mission("Normal", "Nearby System", 1, roll() + roll()
+		return new mission(target, "Normal", "Nearby System", 1, roll() + roll()
 				+ roll(), 0);
 	case "46":
 	case "51":
-		return new mission("Normal", "Nearby System", 1, roll() + roll()
+		return new mission(target, "Normal", "Nearby System", 1, roll() + roll()
 				+ roll(), 1);
 	case "52":
 	case "53":
-		return new mission("Normal", "Nearby System", 1, roll() + roll()
+		return new mission(target, "Normal", "Nearby System", 1, roll() + roll()
 				+ roll() + 2, 1);
 	case "54":
-		return new mission("Normal", "Faraway System", 1, roll() + roll()
+		return new mission(target, "Normal", "Faraway System", 1, roll() + roll()
 				+ roll() + 4, 0);
 	case "55":
-		return new mission("Normal", "Faraway System", 1, roll() + roll()
+		return new mission(target, "Normal", "Faraway System", 1, roll() + roll()
 				+ roll() + 4, 1);
 	case "56":
 	case "61":
-		return new mission("Difficult", "Within System", 2, roll() + roll()
+		return new mission(target, "Difficult", "Within System", 2, roll() + roll()
 				+ roll() + roll(), 1);
 	case "62":
 	case "63":
-		return new mission("Difficult", "Nearby System", 2, roll() + roll()
+		return new mission(target, "Difficult", "Nearby System", 2, roll() + roll()
 				+ roll() + roll() + 2, 0);
 	case "64":
-		return new mission("Difficult", "Nearby System", 2, roll() + roll()
+		return new mission(target, "Difficult", "Nearby System", 2, roll() + roll()
 				+ roll() + roll() + 2, 1);
 	case "65":
-		return new mission("Difficult", "Faraway System", 2, roll() + roll()
+		return new mission(target, "Difficult", "Faraway System", 2, roll() + roll()
 				+ roll() + roll() + roll(), 1);
 	case "66":
-		return new mission("Difficult", "Faraway System", 3, roll() + roll()
+		return new mission(target, "Difficult", "Faraway System", 3, roll() + roll()
 				+ roll() + roll() + roll() + 4, 2);
 	}
 	;
@@ -457,5 +458,372 @@ var complicationAgents = function() {
 	case "66":
 		return new complication( "Intelligence agency involvement","An intelligence agency, possibly factionary, with many resources has the same or opposite goals as the PCs");
 		break;
+	}
+};
+
+
+
+
+//Table 38
+var employerFreeTraders = function(m) {
+	var r = "" + roll() + roll();
+	switch (r) {
+	case "11":
+	case "12":
+	case "13":
+		m.employer = "Outpost Rep";
+		break;
+	case "14":
+	case "15":
+	case "16":
+		m.employer = "Merchant";
+		break;
+	case "21":
+	case "22":
+	case "23":
+		m.employer = "Colonist";
+		break;
+	case "24":
+	case "25":
+	case "26":
+	case "31":
+	case "32":
+	case "33":
+	case "34":
+	case "35":
+	case "36":
+	case "41":
+	case "42":
+	case "43":
+		m.employer = "Patron";
+		break;
+	case "44":
+	case "45":
+	case "46":
+		m.employer = "Trading house";
+		break;
+	case "51":
+	case "52":
+	case "53":
+		m.employer = "Military officer";
+		break;
+	case "54":
+	case "55":
+	case "56":
+		m.employer = "Criminal";
+		break;
+	case "61":
+	case "62":
+	case "63":
+		m.employer = "Factionary";
+		break;
+	case "64":
+	case "65":
+	case "66":
+		m.employer = "Privileged";
+		break;
+	}
+};
+
+// Table 39
+var rewardsFreeTraders = function(m) {
+	var r = "" + roll() + roll();
+	switch (r) {
+	case "11":
+	case "12":
+	case "13":
+		m.reward = "Discount on new cargo";
+		break;
+	case "14":
+	case "15":
+	case "16":
+		m.reward = "New contract";
+		break;
+	case "21":
+	case "22":
+	case "23":
+	case "24":
+	case "25":
+	case "26":
+	case "31":
+	case "32":
+	case "33":
+	case "34":
+	case "35":
+	case "36":
+	case "41":
+	case "42":
+	case "43":
+	case "44":
+	case "45":
+	case "46":
+		m.reward = "Birr";
+		break;
+	case "51":
+	case "52":
+	case "53":
+		m.reward = "Ship module/feature";
+		break;
+	case "54":
+	case "55":
+	case "56":
+		m.reward = "Faction contact";
+		break;
+	case "61":
+	case "62":
+	case "63":
+		m.reward = "Extra debt cancellation";
+		break;
+	case "64":
+	case "65":
+	case "66":
+		m.reward = "Artifact";
+		break;
+	}
+};
+
+// Table 40
+var missionFreeTraders = function(m) {
+	var r = "" + roll() + roll();
+	switch (r) {
+	case "11":
+	case "12":
+		m.mission = "Ore";
+		m.description = "Bauxite (aluminum), lead glance, gold-quartz, cassiterite (tin), malachite (copper), magnetite (iron), platinum-bearing sand, sphalerite (zinc)";
+		break;
+	case "13":
+		m.mission = "Minerals";
+		m.description = "Pure minerals, salt";
+		break;
+	case "14":
+		m.mission = "Metals";
+		m.description = "Bars of metal";
+		break;
+	case "15":
+	case "16":
+		m.mission = "Nobel gasses";
+		m.description = "Argon, helium, neon, krypton, radon, xenon";
+		break;
+	case "21":
+		m.mission = "Timber";
+		m.description = "Pine, jacaranda, mahogany, oak";
+		break;
+	case "22":
+		m.mission = "Ice";
+		m.description = "Water ice or rare frozen solution";
+		break;
+	case "23":
+		m.mission = "Chemicals";
+		m.description = "Industrial chemicals";
+		break;
+	case "24":
+		m.mission = "Fertilizer";
+		m.description = "Natural (droppings) or synthetic";
+		break;
+	case "25":
+	case "26":
+		m.mission = "Textiles";
+		m.description = "Fabrics (wool, velvet, silk) or clothing";
+		break;
+	case "31":
+	case "32":
+		m.mission = "Foodstuffs";
+		m.description = "Base foods such as chickpeas, soybeans and other legumes, grains (corn, barley, rice, wheat) or refined versions thereof (such as couscous) or vegetables such as aubergine, spinach, zucchini, cucumber or tomato";
+		break;
+	case "33":
+		m.mission = "Spices";
+		m.description = "Jasmine, cinnamon, cloves, cumin, mint, basil, oregano, rose water, saffron";
+		break;
+	case "34":
+		m.mission = "Delicacies";
+		m.description = "Apricots, bananas, baklava, dates, coconut, almonds, marzipan, nuts (pecans, pistachios, walnuts), raisins, oysters, caviar, testicles, ants";
+		break;
+	case "35":
+	case "36":
+		m.mission = "Mechanical goods";
+		m.description = "Vaccines, medicines, m- and t-doses, surgical gear for bionics or cybernetics";
+		break;
+	case "41":
+		m.mission = "Religious paraphernalia";
+		m.description = "Altars, prayer towers, dismantled chapels, icons, reliquaries, religious art, talismans, relics, scrolls";
+		break;
+	case "42":
+	case "43":
+		m.mission = "Spare parts";
+		m.description = "Different makes and types, for exos, vehicles, cybernetics, medical technology, prospecting gear, drones, weapons, armor, spaceships or colony life support systems";
+		break;
+	case "44":
+	case "45":
+		m.mission = "Ship parts";
+		m.description = "Modules, features, parts for/from dismantled ships";
+		break;
+	case "46":
+		m.mission = "Exos";
+		m.description = "Exo shells, loader exos, diver exos, carrier exos etc. Stacked effective use of cargo space.";
+		break;
+	case "51":
+		m.mission = "Drones";
+		m.description = "Parts or ready-to-use automatic systems";
+		break;
+	case "52":
+		m.mission = "Vehicles";
+		m.description = "Disassembled";
+		break;
+	case "53":
+	case "54":
+		m.mission = "Weapons";
+		m.description = "Choose from the Gear chapter";
+		break;
+	case "55":
+	case "56":
+		m.mission = "Body armor";
+		m.description = "Choose from the Gear chapter";
+		break;
+	case "61":
+	case "62":
+		m.mission = "Surival gear";
+		m.description = "Basic gear for survival or for a specific environment";
+		break;
+	case "63":
+		m.mission = "Animal feed";
+		m.description = "Alfalfa, tailored animal or livestock feed, synthetic fodder";
+		break;
+	case "64":
+	case "65":
+		m.mission = "Livestock/animals";
+		m.description = "Work animals or pets, usually alive if loaded in a primitive environment or in stasis if from ordinary or advanced environments";
+		break;
+	case "66":
+		m.mission = "Semi-intelligences";
+		m.description = "Ekilibri or nekatra are the most common, the former as plantation workers or pets, the latter as guards, soldiers or gladiators";
+		break;
+	}
+};
+
+
+//Table 41
+var locationFreeTraders = function(m) {
+	var r = "" + roll() + roll();
+	switch (r) {
+	case "11":
+	case "12":
+	case "13":
+		m.location = "Coordinates";
+		m.locationDescription = "Coordinates for an area or secret base.";
+		break;
+	case "14":
+	case "15":
+	case "16":
+		m.location = "Hostile forest/jungles/desert";
+		m.locationDescription = "Inhospitable planet side wilderness. Usually far from cities or settlements.";
+		break;
+	case "21":
+	case "22":
+	case "23":
+	case "24":
+	case "25":
+	case "26":
+		m.location = "Bazaar";
+		m.locationDescription = "A bazaar or market in a city or settlement";
+		break;
+	case "31":
+	case "32":
+	case "33":
+		m.location = "Spaceport";
+		m.locationDescription = "Spaceport on a space station or planet";
+		break;
+	case "34":
+	case "35":
+	case "36":
+		m.location = "Asteroid/Moon";
+		m.locationDescription = "The surface of an asteroid or a smaller moon. Vacuum or dangerous atmosphere";
+		break;
+	case "41":
+	case "42":
+	case "43":
+		m.location = "Space station";
+		m.locationDescription = "On a small space station, anything from an oxygen oasis to a trade or service station";
+		break;
+	case "44":
+	case "45":
+	case "46":
+		m.location = "Soulk";
+		m.locationDescription = "A larger souk where trading partners meet. Usually in a larger city or station, sometimes seasonal";
+		break;
+	case "51":
+	case "52":
+	case "53":
+		m.location = "Colony";
+		m.locationDescription = "A small colony, usually no bigger than a small, sparsely populated village. Far from other settlements.";
+		break;
+	case "54":
+	case "55":
+	case "56":
+		m.location = "Portal station";
+		m.locationDescription = "A portal station next to a sun";
+		break;
+	case "61":
+	case "62":
+	case "63":
+		m.location = "Spaceship";
+		m.locationDescription = "Onboard a spaceship during voyage or docking";
+		break;
+	case "64":
+	case "65":
+	case "66":
+		m.location = "Outpost";
+		m.locationDescription = "A small outpost, usually planet side. An outpost is commonly just a single building with a specific function and only a few inhabitants. For example sensor station, dam control station, courier post or someother communications station";
+		break;
+	}
+};
+
+//Table 23
+var complicationFreeTraders = function() {
+	var r = "" + roll() + roll();
+	switch (r) {
+	case "11":
+	case "12":
+	case "13":
+		return new complication( "Embargo/Quarantine","The destination is placed under quarantine or an embargo.");
+	case "14":
+	case "15":
+	case "16":
+		return new complication( "Customs inspection","A tolling vessel or some other customs ship approaches and demands to be allowed to board.");
+	case "21":
+	case "22":
+	case "23":
+	case "24":
+	case "25":
+	case "26":
+		return new complication( "Corrupt customs officer","A customs officer demands to be bribed to allow the PCs through or will make sure their cargo gets tangled up in bureaucracy.");
+	case "31":
+	case "32":
+	case "33":
+		return new complication( "Corsairs","Corsairs or pirates attack the group's work site or ship.");
+	case "34":
+	case "35":
+	case "36":
+	case "41":
+	case "42":
+	case "43":
+		return new complication( "Break-down","Some form of breakdown occurs onboard, during either loading or unloading.");
+	case "44":
+	case "45":
+	case "46":
+	case "51":
+	case "52":
+	case "53":
+		return new complication( "Lost","The group strays from their planned route and is lost on a planet or in space.");
+	case "54":
+	case "55":
+	case "56":
+		return new complication( "Tracky merchant","The PCs encounter a merchant who hinders them from selling at a profit or tricks them in some other way.");
+	case "61":
+	case "62":
+	case "63":
+	case "64":
+	case "65":
+	case "66":
+		return new complication( "Saturated market","The market at the destination recently received a big shipment of goods similar to the PCs' and the prices have dropped.");
 	}
 };
